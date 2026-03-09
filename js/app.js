8dd834b9d86384d781d267b5c4b1eef1235d1f8a
CLerdmy <podturov@gmail.com>
2026-03-10 00:16:07 +0300

    update app

---
+++ js/app.js
@@ -0,0 +1,60 @@
+import { renderPage } from "./router.js";
+import { state } from "./state.js";
+
+function renderHeader(){
+
+    const header = document.getElementById("header");
+
+    header.innerHTML = `
+
+    <div class="header-grid">
+
+        <div class="progress">Уровней изучено: ${state.completedLevels}/5</div>
+
+        <h1 class="title">
+            PostgreSQL Monitoring Tutorial
+        </h1>
+
+        <div></div>
+
+    </div>
+
+    <div class="level-nav">
+
+        <button data-page="intro">Введение</button>
+        <button data-page="level1">Уровень 1</button>
+        <button data-page="level2">Уровень 2</button>
+        <button data-page="level3">Уровень 3</button>
+        <button data-page="level4">Уровень 4</button>
+        <button data-page="level5">Уровень 5</button>
+        <button data-page="final">Финал</button>
+
+    </div>
+
+    `;
+
+    document.querySelectorAll("[data-page]").forEach(btn => {
+
+        btn.addEventListener("click", () => {
+
+            const page = btn.dataset.page;
+
+            state.currentPage = page;
+
+            renderPage(page);
+            renderHeader();
+
+        });
+
+    });
+
+}
+
+function init(){
+
+    renderHeader();
+    renderPage(state.currentPage);
+
+}
+
+init();
