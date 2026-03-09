a8ab59e1258c3b4e54eb4e983017003b95cf1637
CLerdmy <podturov@gmail.com>
2026-03-10 00:16:13 +0300

    add state

---
+++ js/state.js
@@ -0,0 +1,22 @@
+export const state = {
+
+    currentPage: "intro",
+
+    completedLevels: 0,
+
+    unlockedLevels: [
+        "intro",
+        "level1"
+    ],
+
+    quizAnswers: {
+
+        level1: {},
+        level2: {},
+        level3: {},
+        level4: {},
+        level5: {}
+
+    }
+
+};
