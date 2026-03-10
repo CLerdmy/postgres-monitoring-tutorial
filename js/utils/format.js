91e6a32eb0330511b4a2c459eaf8d3ad7ad5a099
CLerdmy <podturov@gmail.com>
2026-03-10 23:33:32 +0300

    refactor format

--- js/utils/format.js
+++ js/utils/format.js
@@ -4,6 +4,11 @@ export function formatText(text, strong = [], code = []) {
 
     let result = text;
 
+    const CODE_START = "\uE000";
+    const CODE_END = "\uE001";
+    const STRONG_START = "\uE002";
+    const STRONG_END = "\uE003";
+
     const escapeRegExp = (str) =>
         str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
@@ -16,7 +21,7 @@ export function formatText(text, strong = [], code = []) {
 
         const reg = new RegExp(escapeRegExp(word), "g");
 
-        result = result.replace(reg, `@@CODE_START@@${word}@@CODE_END@@`);
+        result = result.replace(reg, `${CODE_START}${word}${CODE_END}`);
     });
 
     strong.forEach(word => {
@@ -25,14 +30,14 @@ export function formatText(text, strong = [], code = []) {
 
         const reg = new RegExp(escapeRegExp(word), "g");
 
-        result = result.replace(reg, `@@STRONG_START@@${word}@@STRONG_END@@`);
+        result = result.replace(reg, `${STRONG_START}${word}${STRONG_END}`);
     });
 
     result = result
-        .replace(/@@CODE_START@@/g,"<code>")
-        .replace(/@@CODE_END@@/g,"</code>")
-        .replace(/@@STRONG_START@@/g,"<strong>")
-        .replace(/@@STRONG_END@@/g,"</strong>");
+        .replace(new RegExp(CODE_START, "g"), "<code>")
+        .replace(new RegExp(CODE_END, "g"), "</code>")
+        .replace(new RegExp(STRONG_START, "g"), "<strong>")
+        .replace(new RegExp(STRONG_END, "g"), "</strong>");
 
     return result;
 }
